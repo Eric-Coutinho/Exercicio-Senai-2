75d78289116e8d37eaabd16fac76ac35e6405e10
eric-coutinho <eric.coutinhop@hotmail.com>
2024-04-04 10:11:10 -0300

    Finished raw styles of product showroom, started to work on rick and morty characters page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Card } from './components/Card'
 import produtos from './constants/produtos.json'
 import { api } from "./api/rmApi"
 import style from './App.module.css'
+import { CardRaM } from './components/CardRaM'
 
 function App() {
   const [show, setShow] = useState("")
@@ -32,14 +33,14 @@ function App() {
       <button onClick={() => setShow("map")}>Mapa</button>
     </div>
     <div  className={style.wrapPage}>
-      <h1>Exercícios de manutenção</h1>
+      <h1 className={style.title}>Exercícios de manutenção</h1>
      {show === "prod" &&
         <>
-          <h2>Showroom de produtos</h2>
-            <div>
+          <h2 className={style.title}>Showroom de produtos</h2>
+            <div className={style.cardContainer}>
             {produtos.map((item) => {
               return(
-                <Card name={item.name} desc={item.desc} value={item.value} image={item.image} key={item.id}/>
+                <Card name={item.name} desc={item.desc} value={item.value} image={item.image} category={item.category} status={item.status} key={item.id}/>
               )
              })}
             </div>
@@ -51,11 +52,11 @@ function App() {
             <div>
                <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)}/>
             </div>
-            <div>
+            <div className={style.cardContainer}>
             {data.map((item) => { 
              return(
               <div key={item.id}>
-                <Card name={item.name} desc={item.species} value={item.gender} image={item.image} />
+                <CardRaM name={item.name} species={item.species} status={item.status} type={item.type} gender={item.gender} image={item.image} />
                 {/* <button onClick={() => {}}>Info</button> */}
               </div>
               )
--- src/components/CardRaM.jsx
+++ src/components/CardRaM.jsx
@@ -1,25 +1,16 @@
 /* eslint-disable react/prop-types */
 import * as bootstrap from 'bootstrap'
 import style from './CardStyles.module.css'
-
-export const Card = ({ name, desc, value, image, category, status }) => {
-  var statusElement = ""
-  if (status == "true") {
-    statusElement = "🟢"
-  }
-  else {
-    statusElement = "🔴"
-  }
-
+export const CardRaM = ({ name, status, species, type, gender, image }) => {
   return (
     <div className={style.card}>
       <h1>{name}</h1>
-      <p>{category}</p>
-      <h2>{desc}</h2>
-      <p>{value}</p>
+      <h2>{species}</h2>
+      <p>{status}</p>
+      <p>{type}</p>
+      <p>{gender}</p>
       <div className={style.image}>
         <img src={image} alt={name} width={300} height={200} className={style.img}/>
-        <p className={style.cardOverlay}>{statusElement}</p>
       </div>
     </div>
   )
